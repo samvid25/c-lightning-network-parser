da7d92e0a1f487244cb3d80e4fc663fc5d4d15b9
Samvid Dharanikota <samvid25@yahoo.com>
2019-06-08 22:48:18 +0400

    Add histograms, move sigmajs code to new branch

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # C-lightning Network Graph Parser
+
+This is a simple script to parse a Lightning Network graph (obtained from the c-lightning's `listchannels`).
+
+The scripts are by no means optimized, and are just intended to perform what is required.
--- graph.py
+++ graph.py
@@ -2,6 +2,7 @@ import json
 import networkx as nx
 from lightning import LightningRpc
 import subprocess
+import matplotlib.pyplot as plt
 
 graph = nx.DiGraph()
 
@@ -22,4 +23,20 @@ def import_graph():
 		graph.add_edge(channel["source"], channel["destination"], satoshis=channel["satoshis"], base_fee_millisatoshi=channel["base_fee_millisatoshi"], fee_per_millionth=channel["fee_per_millionth"], delay=channel["delay"], short_channel_id=channel["short_channel_id"])
 	print (graph.edges())
 
+def base_fee_hist():
+	base_fee_list = []
+	for u, v, bf in graph.edges(data='base_fee_millisatoshi'):
+		base_fee_list.append(bf)
+	plt.hist(base_fee_list)
+	plt.show()
+
+def delay_hist():
+	delay_list = []
+	for u, v, d in graph.edges(data='delay'):
+		delay_list.append(d)
+	plt.hist(delay_list)
+	plt.show()
+
 import_graph()
+base_fee_hist()
+delay_hist()
